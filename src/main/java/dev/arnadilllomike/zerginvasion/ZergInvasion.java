c1ece5bd96b21a0d4416a269c94977056f182b55
JacksonWengler <jacksonwengler@gmail.com>
2025-05-20 17:18:40 -0500

    Initial commit

---
+++ src/main/java/dev/arnadilllomike/zerginvasion/ZergInvasion.java
@@ -0,0 +1,16 @@
+package dev.arnadilllomike.zerginvasion;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ZergInvasion implements ModInitializer {
+	public static final String MOD_ID = "zerginvasion";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/dev/arnadilllomike/zerginvasion/ZergInvasionClient.java
@@ -0,0 +1,10 @@
+package dev.arnadilllomike.zerginvasion;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ZergInvasionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
